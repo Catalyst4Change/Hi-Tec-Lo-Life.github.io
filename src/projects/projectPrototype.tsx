fc2bfb49f2ec2b05112bc96e4fc31e8e49d2033d
Catalyst4Change <catalyst4change@users.noreply.github.com>
2026-06-26 08:15:40 -0600

    add project images

--- src/projects/projectPrototype.tsx
+++ src/projects/projectPrototype.tsx
@@ -17,7 +17,7 @@ export const projectItems: ProjectItem[] = [
     name: 'The Sigil',
     type: 'document',
     icon: textFile,
-    images: ['https://hi-tec-lo-life.com/images/sigil.jpeg'],
+    images: ['src/assets/images/sigil.JPEG'],
     content:
       '>As our camp continued to gain a presence and an aesthetic, we wanted to create a symbol that represented our identity and values.\n\n>We settled on the Unicursal Hexagram - a six-pointed star drawn in one unbroken line.\n\n>It, like us, has a cultish vibe and is reminiscent of sacred geometry. Once used by infamous occultist Aleister Crowley, the symbol has a rich history dating back to the 16th century.The symbol was also used by Italian philosopher Giordano Bruno, who was burned at the stake by the Roman Catholic Church for his "heretical ideas", such as the Su\n\n>n being the center of our solar system and that other planets could potentially harbor life.\n\n>We felt that this rebellious and non-conformist spirit aligned with our own values and identity.\n\n>Anyway, we built a big one out of reclaimed aluminum and backlit it with addressable LEDs. The math required to calculate the angles and dimensions of the hexagram was a fun challenge, and we ended up with a striking and unique piece of art that represents Hi-Tec_Lo-Life and its values.',
   },
@@ -26,7 +26,7 @@ export const projectItems: ProjectItem[] = [
     name: 'HexaBench',
     type: 'document',
     icon: textFile,
-    images: ['https://hi-tec-lo-life.com/images/hexabench.jpeg', 'https://hi-tec-lo-life.com/images/hexabench_backyard.jpeg'],
+    images: ['src/assets/images/hexa-bench-1.JPEG', 'src/assets/images/hexa-bench-2.JPEG'],
     content:
       'Flat-pack Hex-a-Bench\n\n>When thinking of futuristic furniture for outdoor use, we wanted something that would be portable, easy to assemble, and most importantly - look sick as hell.\n\n>I ended up teaching myself how to use CAD software and designed a hexagonal bench where each seating section can be cut from a single plywood sheet.\n\n>We hand-cut a prototype bench and realized weight distribution and stability were issues. After some adjustments, we sent the plans to a CnC mill where it was cut out.\n\n>The final product is a bench that can be easily dis/assembled without any tools or hardware and packed flat for transport, making it perfect for outdoor events.\n\n>Oh, and of course we painted it black so it looks sick as hell.',
   },
@@ -44,7 +44,7 @@ export const projectItems: ProjectItem[] = [
     name: 'Trip Tent',
     type: 'document',
     icon: textFile,
-    images: [projectImagePlaceholder],
+    images: ['src/assets/images/trip-tent.JPEG'],
     content:
       '404 - Project not found',
   },
